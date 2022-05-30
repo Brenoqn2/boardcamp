74bae5cee2ae4ea0a7988283cac9eaeb476eb797
Brenoqn2 <brenoqn2@gmail.com>
2022-05-30 05:54:15 -0300

    added feature 'getRentals'

---
+++ controllers/rentals.js
@@ -0,0 +1,44 @@
+import db from "../database.js";
+export async function getRentals(req, res) {
+  try {
+    const customerId = req.query.customerId;
+    const gameId = req.query.gameId;
+    let result;
+    if (customerId) {
+      result = await db.query(
+        `SELECT * FROM rentals WHERE customerId LIKE $1`,
+        [`${customerId}%`]
+      );
+      result = result.rows;
+    } else if (gameId) {
+      result = await db.query(
+        `SELECT * FROM rentals WHERE customerId LIKE $1`,
+        [`${customerId}%`]
+      );
+      result = result.rows;
+    } else {
+      result = await db.query(`SELECT * FROM rentals`);
+      result = result.rows;
+    }
+    let responseArr = [];
+    result.forEach(async (rental) => {
+      let customerInfo = await db.query(
+        `SELECT * FROM customers WHERE id = ${rental.customerId}`
+      );
+      customerInfo = customerInfo.rows;
+      let gameInfo = await db.query(
+        `SELECT * FROM games WHERE id = ${rental.gameId}`
+      );
+      gameInfo = gameInfo.rows;
+      responseArr.push({
+        ...rental,
+        customer: { ...customerInfo[0] },
+        game: { ...gameInfo[0] },
+      });
+    });
+    res.send(responseArr);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+}
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotenv.config();
 import categoriesRouter from "./routes/categories.js";
 import gamesRouter from "./routes/games.js";
 import customersRouter from "./routes/customers.js";
+import rentalsRouter from "./routes/rentals.js";
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(categoriesRouter);
 app.use(gamesRouter);
 app.use(customersRouter);
+app.use(rentalsRouter);
 
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
